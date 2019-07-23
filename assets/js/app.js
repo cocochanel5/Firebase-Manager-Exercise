e4d594c5f0da05763c76a6a405209592aa1b340c
Alex <alex@apresnail.com>
2019-07-22 19:03:12 -0700

    added firebase in js

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.3.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->
+
+<script>
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDox-CRhd03aRqPfw92tFNG_top96u4-Mg",
+    authDomain: "fir-manager-exercise.firebaseapp.com",
+    databaseURL: "https://fir-manager-exercise.firebaseio.com",
+    projectId: "fir-manager-exercise",
+    storageBucket: "fir-manager-exercise.appspot.com",
+    messagingSenderId: "713777060931",
+    appId: "1:713777060931:web:07ffddf8cfb44eb6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+  
+</script>
