b06f60287b371f038126bbb2d60eafc6ddb6b057
Alex <alex@apresnail.com>
2019-07-22 19:50:30 -0700

    added inputs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,60 @@
 
-$( document ).ready(function() {
+$(document).ready(function () {
 
 
 
     var firebaseConfig = {
         apiKey: "AIzaSyDox-CRhd03aRqPfw92tFNG_top96u4-Mg",
-    authDomain: "fir-manager-exercise.firebaseapp.com",
-    databaseURL: "https://fir-manager-exercise.firebaseio.com",
-    projectId: "fir-manager-exercise",
-    storageBucket: "fir-manager-exercise.appspot.com",
-    messagingSenderId: "713777060931",
-    appId: "1:713777060931:web:07ffddf8cfb44eb6"
-  };
+        authDomain: "fir-manager-exercise.firebaseapp.com",
+        databaseURL: "https://fir-manager-exercise.firebaseio.com",
+        projectId: "fir-manager-exercise",
+        storageBucket: "fir-manager-exercise.appspot.com",
+        messagingSenderId: "713777060931",
+        appId: "1:713777060931:web:07ffddf8cfb44eb6"
+    };
 
 
-  firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
 
-var employeeName = employeeName;
-var monthlyRate = monthlyRate;
-var role = role;
-var startDate = startDate;
+    var database = firebase.database();
+
+    var employeeName = "";
+
+    var role = "";
+
+    var startDate = "";
+
+    var monthlyRate = "";
 
 
-$("#submit").on("click", function(event){
+
+
+
+
+
+
+    $("#submit").on("click", function (event) {
         event.preventDefault();
 
         alert("test")
+
+        employeeName = $("#employeeName").val().trim();
+
+        role = $("#role").val().trim();
+
+        startDate = $("#startDate").val().trim();
+
+        monthlyRate = $("#monthlyRate").val().trim();
+
+        database.ref().push({
+            employeeName: employeeName,
+            role: role,
+            startDate: startDate,
+            monthlyRate: monthlyRate,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+
     });
-    
-    
+
+
 });
